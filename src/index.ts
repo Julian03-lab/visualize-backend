61cb9f6e1b1ed27c665cc2536c39c5ced0383a92
Julian <julian03yt@gmail.com>
2024-07-08 00:10:14 -0300

    chore: Update package.json scripts and remove unused dependencies

--- src/index.ts
+++ src/index.ts
@@ -145,6 +145,6 @@ app.post("/create-timelapse", async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
